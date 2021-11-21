2726581d72e3bc97cd326cec4aff45fec4a3d242
keitsune <tsune.kei.32821@gmail.com>
2021-11-21 09:03:42 +0000

    [Add] genre編集ページの追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,19 +1,26 @@
 class Admin::GenresController < ApplicationController
   def index
-    @genre = Genre.new
+    @genre_new = Genre.new
     @genres = Genre.all
   end
 
   def create
     genre = Genre.new(genre_params)
     genre.save
-    redirect_to genres_path
+    redirect_to admin_genres_path
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      redirect_to admin_genres_path
+    else
+      render :edit
+    end
   end
 
   private
