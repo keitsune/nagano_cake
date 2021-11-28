0c8883a683847de0535243a6a51641cdef56f7ed
keitsune <tsune.kei.32821@gmail.com>
2021-11-28 12:34:10 +0000

    [Add] 商品詳細、編集ページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
 
-  devise_for :customers
+
+
+  devise_for :customers, path:""
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items, only: [:new, :index, :create, :show, :edit, :update]
+  end
+
+  scope module: :public do
+    root 'homes#top'
+    resource :customers, only: [:show, :edit, :update, :unsubscribe, :withdraw]
+    get "customers/unsubscribe" => "public/customers#unsubscribe"
   end
 end
