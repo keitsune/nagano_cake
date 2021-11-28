dc961cef50f1e8036dc9f7111073568bbe838ddd
keitsune <tsune.kei.32821@gmail.com>
2021-11-28 12:34:10 +0000

    [Add] 会員詳細、編集ページ追加

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,21 @@
+class Public::CustomersController < ApplicationController
+  def show
+    @customer = current_customer
+  end
+
+  def edit
+    @customer = current_customer
+  end
+
+  def update
+    
+  end
+
+  def unsubscribe
+  end
+
+  def withdraw
+  end
+
+
+end
